b470956038a1b9ad05af0b3a22f17f1654bebdc4
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2017-07-23 05:16:36 -0400

    Change Game state to Menu state.

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import Phaser from 'phaser'
 
 import BootState from './states/Boot'
 import SplashState from './states/Splash'
-import GameState from './states/Game'
+import MenuState from './states/Menu'
 import ROT from './lib/ROT'
 
 import {getHeight, getWidth} from './utils/DOM'
@@ -20,7 +20,7 @@ class Game extends Phaser.Game {
 
     this.state.add('Boot', BootState, false)
     this.state.add('Splash', SplashState, false)
-    this.state.add('Game', GameState, false)
+    this.state.add('Menu', MenuState, false)
 
     this.state.start('Boot')
   }
